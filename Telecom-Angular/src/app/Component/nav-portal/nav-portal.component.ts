59372031a0036602d9241fb63e8e4a3de412bcf2
rtayl126 <93009189+rtaylor126@users.noreply.github.com>
2021-11-04 18:34:45 -0400

    Logout function

--- Telecom-Angular/src/app/Component/nav-portal/nav-portal.component.ts
+++ Telecom-Angular/src/app/Component/nav-portal/nav-portal.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/Services/authentication.service';
 
 @Component({
   selector: 'app-nav-portal',
   templateUrl: './nav-portal.component.html',
   styleUrls: ['./nav-portal.component.css']
 })
-export class NavPortalComponent implements OnInit {
+export class NavPortalComponent implements OnInit, OnDestroy {
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private _auth: AuthenticationService) { }
+  ngOnDestroy(): void {
+    this._auth.logOut();
   }
 
-  logOut(){
-    localStorage.removeItem('currentUser');
-    this.router.navigate(['app-portal'])
+  ngOnInit(): void {
   }
 
 }
